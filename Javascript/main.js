d34f4db63bb709e24415b50fb801251d0a214ab4
BishalMrzn <hapiebishal5@gmail.com>
2024-06-20 14:38:03 +0545

    first commit

---
+++ Javascript/main.js
@@ -0,0 +1,5 @@
+console.log("hello")
+let a=2389124892389472389578942182472385780235789897;//start with a,$ or _
+console.log(a)
+a="thank you"
+console.log(a)
---
+++ Javascript/variable.js
@@ -0,0 +1,17 @@
+//const value doesn't change
+//let is used to declare variable
+//var was used in es6 and before
+console.log("Javascript tutorial for variable")
+let a=202;
+const author="hello"
+// let js=2343 shows error
+// author=2    const variable assign error
+a=2
+let b="Bishal"
+let c=null;
+let d=undefined
+{
+    let b=9
+    console.log(b)
+}
+console.log(b)
